2d4c96145a026011dea37678995bd01ae16715be
Dmitry Shagurov <shagurovda@gmail.com>
2022-08-02 17:13:22 +0400

    highlight for active tag in Header nav

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,20 +1,36 @@
+import classNames from "classnames";
 import { ThemeSwitchButton } from "src/components";
 import { useAppSelector } from "src/redux/hooks";
+import { NavigationPage } from "src/redux/slices/browseSlice";
 import styles from "./Header.module.sass";
 
 const Header = () => {
-  const activePage = useAppSelector((state) => state.browse.activePage)
+  const activePage = useAppSelector((state) => state.browse.activePage);
+
   return (
     <div className={styles.body}>
       <div className={`wrapper ${styles.wrapper}`}>
         <div className={styles.logo}>logo</div>
         <div className={styles.interactive}>
           <nav className={styles.navigation}>
-            <a className={styles.link} href="#">Главная</a>
-            <a className={styles.link} href="#">Тэги</a>
-            <a className={styles.link} href="#">Опросы</a>
+
+            <a href="#" className={classNames(
+              styles.link,
+              { [styles.activeLink]: activePage === NavigationPage.Main }
+            )}>Главная</a>
+
+            <a href="#" className={classNames(
+              styles.link,
+              { [styles.activeLink]: activePage === NavigationPage.Tags }
+            )}>Опросы</a>
+
+            <a href="#" className={classNames(
+              styles.link,
+              { [styles.activeLink]: activePage === NavigationPage.QuestionnaireList }
+            )}>Тэги</a>
+
           </nav>
-          <ThemeSwitchButton/>
+          <ThemeSwitchButton />
           <div>user</div>
         </div>
       </div>
