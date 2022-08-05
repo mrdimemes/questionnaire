cb970dade05297bea3a6ba2c55ab42f870e53ab3
Dmitry Shagurov <shagurovda@gmail.com>
2022-08-05 15:20:08 +0400

    header/ThemeSwitchButton component tests

---
+++ src/components/Header/ThemeSwitchButton/ThemeSwitchButton.test.tsx
@@ -0,0 +1,25 @@
+import { screen } from "@testing-library/react";
+import userEvent from "@testing-library/user-event";
+import { renderWithProviders } from "src/utils/test-utils";
+import { setupStore } from "src/redux/store";
+import { Theme } from "src/redux/slices/themeSlice";
+import ThemeSwitchButton from "./ThemeSwitchButton";
+
+describe("Header/ThemeSwitchButton component", () => {
+  test("ThemeSwitchButton renders", () => {
+    renderWithProviders(<ThemeSwitchButton />);
+    expect(screen.getByRole("button")).toBeInTheDocument();
+  });
+
+  test("ThemeSwitchButton switches the app's theme", () => {
+    const store = setupStore({ theme: { theme: Theme.Light } })
+    renderWithProviders(<ThemeSwitchButton />, { store });
+    userEvent.click(screen.getByRole("button"));
+    expect(store.getState().theme.theme).toEqual(Theme.Dark);
+  });
+
+  test("ThemeSwitchButton snapshot", () => {
+    const switchBtn = renderWithProviders(<ThemeSwitchButton />);
+    expect(switchBtn).toMatchSnapshot();
+  });
+});
