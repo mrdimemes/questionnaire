07495e8eff697b64dcbd503b4cd09e6cb75cc24e
Dmitry Shagurov <shagurovda@gmail.com>
2022-08-05 13:56:44 +0400

    utils for testing components connected to redux store

--- src/redux/store.ts
+++ src/redux/store.ts
@@ -1,15 +1,24 @@
-import { configureStore } from "@reduxjs/toolkit";
+import { combineReducers, configureStore } from "@reduxjs/toolkit";
+import type { PreloadedState } from '@reduxjs/toolkit'
 import { authSlice, tagsSlice, browseSlice, themeSlice } from "./slices";
 
-const store = configureStore({
-  reducer: {
-    tags: tagsSlice,
-    browse: browseSlice,
-    auth: authSlice,
-    theme: themeSlice
-  }
-})
+const rootReducer = combineReducers({
+  tags: tagsSlice,
+  browse: browseSlice,
+  auth: authSlice,
+  theme: themeSlice
+});
+
+export const setupStore = (preloadedState?: PreloadedState<RootState>) => {
+  return configureStore({
+    reducer: rootReducer,
+    preloadedState
+  })
+}
+
+const store = setupStore();
 
 export default store;
-export type RootState = ReturnType<typeof store.getState>;
-export type AppDispatch = typeof store.dispatch;
+export type RootState = ReturnType<typeof rootReducer>;
+export type AppStore = ReturnType<typeof setupStore>;
+export type AppDispatch = AppStore['dispatch'];
---
+++ src/utils/test-utils.tsx
@@ -0,0 +1,24 @@
+import React, { PropsWithChildren } from 'react'
+import { render, RenderOptions } from '@testing-library/react'
+import { configureStore, PreloadedState } from '@reduxjs/toolkit'
+import { Provider } from "react-redux"
+import { setupStore, AppStore, RootState } from "src/redux/store";
+
+interface ExtendedRenderOptions extends Omit<RenderOptions, 'queries'> {
+  preloadedState?: PreloadedState<RootState>
+  store?: AppStore
+}
+
+export function renderWithProviders(
+  ui: React.ReactElement,
+  {
+    preloadedState = {},
+    store = setupStore(preloadedState),
+    ...renderOptions
+  }: ExtendedRenderOptions = {}
+) {
+  function Wrapper({ children }: PropsWithChildren<{}>): JSX.Element {
+    return <Provider store={store}>{children}</Provider>
+  }
+  return { store, ...render(ui, { wrapper: Wrapper, ...renderOptions }) }
+}
