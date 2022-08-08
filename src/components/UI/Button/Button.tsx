ef6a7b93b4c44e9d5b0d9283dac664f1434e26f2
Dmitry Shagurov <shagurovda@gmail.com>
2022-08-08 13:49:21 +0400

    themes for UI/Button

--- src/components/UI/Button/Button.tsx
+++ src/components/UI/Button/Button.tsx
@@ -1,5 +1,7 @@
 import React from "react";
 import classNames from "classnames";
+import { useAppSelector } from "src/redux/hooks";
+import { Theme } from "src/redux/slices/themeSlice";
 import styles from "./Button.module.sass";
 
 type ButtonProps = {
@@ -9,13 +11,21 @@ type ButtonProps = {
 }
 
 const Button = ({ children, onClick, className }: ButtonProps) => {
+  const currentTheme = useAppSelector((state) => state.theme.theme);
   const onClickHandler = (_event: React.MouseEvent<HTMLElement>) => {
     onClick();
   }
 
   return (
     <button
-      className={classNames(styles.body, className)}
+      className={classNames(
+        styles.body,
+        {
+          [styles.theme_light]: currentTheme === Theme.Light,
+          [styles.theme_dark]: currentTheme === Theme.Dark
+        },
+        className
+      )}
       onClick={onClickHandler}
     >
       {children}
