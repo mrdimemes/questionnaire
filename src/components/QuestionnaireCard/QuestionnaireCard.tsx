2e62185f579d39dab7c32f3e15324bb992c5e0ad
Dmitry Shagurov <shagurovda@gmail.com>
2022-08-12 13:10:55 +0400

    basic hover effect for questionnaire cards; cursor styles along existing pages

--- src/components/QuestionnaireCard/QuestionnaireCard.tsx
+++ src/components/QuestionnaireCard/QuestionnaireCard.tsx
@@ -1,3 +1,6 @@
+import classNames from "classnames";
+import { useAppSelector } from "src/redux/hooks";
+import { Theme } from "src/redux/slices/themeSlice";
 import { Tag } from "src/components";
 import styles from "./QuestionnaireCard.module.sass";
 
@@ -11,7 +14,15 @@ type QuestionnaireCardProps = {
 }
 
 const QuestionnaireCard = ({ id, label, tags }: QuestionnaireCardProps) => {
-  return <div>
+  const currentTheme = useAppSelector((state) => state.theme.theme);
+
+  return <div className={classNames(
+    styles.body,
+    {
+      [styles.theme_dark]: currentTheme === Theme.Dark,
+      [styles.theme_light]: currentTheme === Theme.Light
+    }
+  )}>
     <h2 className={styles.label}>{label}</h2>
     <div className={styles.tags}>{
       tags.map((tag) => {
