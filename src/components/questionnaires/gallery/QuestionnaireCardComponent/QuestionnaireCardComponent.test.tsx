d3368c9385a49040b4c159ca00a347fcc60ec17d
Dmitry Shagurov <shagurovda@gmail.com>
2022-08-29 15:50:31 +0400

    wrapper questionnaire card component into React Router Link; affected tests & snapshot upd

--- src/components/questionnaires/gallery/QuestionnaireCardComponent/QuestionnaireCardComponent.test.tsx
+++ src/components/questionnaires/gallery/QuestionnaireCardComponent/QuestionnaireCardComponent.test.tsx
@@ -1,23 +1,27 @@
 import { screen } from "@testing-library/react";
-import { renderWithProviders } from "src/utils/test-utils";
+import { renderWithProvidersAndRouters } from "src/utils/test-utils";
 import QuestionnaireCardComponent from "./QuestionnaireCardComponent";
 
 describe("QuestionnaireCard component", () => {
 
   test("QuestionnaireCard renders", () => {
-    renderWithProviders(<QuestionnaireCardComponent
-      label={"Question123"}
-      tags={[{ id: 1, label: "Tag123" }]}
-    />);
+    renderWithProvidersAndRouters(
+      <QuestionnaireCardComponent
+        id={1}
+        label={"Question123"}
+        tags={[{ id: 1, label: "Tag123" }]}
+      />);
     expect(screen.getByText(/question123/i)).toBeInTheDocument();
     expect(screen.getByText(/tag123/i)).toBeInTheDocument();
   });
 
   test("QuestionnaireCard snapshot", () => {
-    const { container } = renderWithProviders(<QuestionnaireCardComponent
-      label={"Question123"}
-      tags={[{ id: 1, label: "Tag123" }]}
-    />);
+    const { container } = renderWithProvidersAndRouters(
+      <QuestionnaireCardComponent
+        id={1}
+        label={"Question123"}
+        tags={[{ id: 1, label: "Tag123" }]}
+      />);
     expect(container).toMatchSnapshot();
   });
 });
--- src/components/questionnaires/gallery/QuestionnaireCardComponent/QuestionnaireCardComponent.tsx
+++ src/components/questionnaires/gallery/QuestionnaireCardComponent/QuestionnaireCardComponent.tsx
@@ -1,10 +1,12 @@
 import classNames from "classnames";
+import { Link } from "react-router-dom";
 import { useAppSelector } from "src/redux/hooks";
 import { getThemeStyle } from "src/redux/slices/themeSlice";
 import { TagBar } from "src/components";
 import styles from "./QuestionnaireCardComponent.module.sass";
 
 type QuestionnaireCardProps = {
+  id: number,
   label: string,
   tags: {
     id: number,
@@ -12,16 +14,22 @@ type QuestionnaireCardProps = {
   }[]
 }
 
-const QuestionnaireCardComponent = ({ label, tags }: QuestionnaireCardProps) => {
+const QuestionnaireCardComponent = ({
+  id,
+  label,
+  tags }: QuestionnaireCardProps
+) => {
   const currentTheme = useAppSelector((state) => state.theme.theme);
 
-  return <div className={classNames(
-    styles.body,
-    getThemeStyle(styles, currentTheme)
-  )}>
-    <h2 className={styles.label}>{label}</h2>
-    <TagBar tags={tags} />
-  </div>
+  return (
+    <Link
+      to={"/questionnaire/" + id}
+      className={classNames(styles.body, getThemeStyle(styles, currentTheme))}
+    >
+      <h2 className={styles.label}>{label}</h2>
+      <TagBar tags={tags} />
+    </Link>
+  )
 }
 
 export default QuestionnaireCardComponent
