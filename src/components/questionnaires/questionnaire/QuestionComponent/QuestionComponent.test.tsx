3ae0252f0e5a51daa82fe17f4575cbe1d16d8c83
Dmitry Shagurov <shagurovda@gmail.com>
2022-08-31 13:41:45 +0400

    QuestionComponent tests

---
+++ src/components/questionnaires/questionnaire/QuestionComponent/QuestionComponent.test.tsx
@@ -0,0 +1,68 @@
+import { screen } from "@testing-library/react";
+import { renderWithProviders } from "src/utils/test-utils";
+import QuestionComponent from "./QuestionComponent";
+import { QuestionType } from "src/models/questionnaire/QuestionType";
+
+const fieldsPlaceholder = [
+  { id: 1, text: "field1" },
+  { id: 2, text: "field2" }
+]
+
+describe("Question component", () => {
+  test("Checkbox question renders", () => {
+    renderWithProviders(
+      <QuestionComponent
+        id={1}
+        questionType={QuestionType.Checkbox}
+        text="test"
+        fields={fieldsPlaceholder}
+        isRequired={false}
+      />);
+    expect(screen.getByText(/test/i)).toBeInTheDocument();
+    expect(screen.getByText(/field1/i)).toBeInTheDocument();
+    expect(screen.getByText(/field2/i)).toBeInTheDocument();
+    expect(screen.getAllByRole("checkbox")).toHaveLength(2);
+  });
+
+  test("Radio question renders", () => {
+    renderWithProviders(
+      <QuestionComponent
+        id={1}
+        questionType={QuestionType.Radio}
+        text="test"
+        fields={fieldsPlaceholder}
+        isRequired={false}
+      />);
+    expect(screen.getByText(/test/i)).toBeInTheDocument();
+    expect(screen.getByText(/field1/i)).toBeInTheDocument();
+    expect(screen.getByText(/field2/i)).toBeInTheDocument();
+    expect(screen.getAllByRole("radio")).toHaveLength(2);
+  });
+
+  test("Text question renders", () => {
+    renderWithProviders(
+      <QuestionComponent
+        id={1}
+        questionType={QuestionType.Text}
+        text="test"
+        fields={fieldsPlaceholder}
+        isRequired={false}
+      />);
+    expect(screen.getByText(/test/i)).toBeInTheDocument();
+    expect(screen.getByText(/field1/i)).toBeInTheDocument();
+    expect(screen.getByText(/field2/i)).toBeInTheDocument();
+    expect(screen.getAllByRole("textbox")).toHaveLength(2);
+  });
+
+  test("Question snapshot", () => {
+    const question = renderWithProviders(
+      <QuestionComponent
+        id={1}
+        questionType={QuestionType.Checkbox}
+        text="test"
+        fields={fieldsPlaceholder}
+        isRequired={false}
+      />);
+    expect(question).toMatchSnapshot();
+  });
+});
