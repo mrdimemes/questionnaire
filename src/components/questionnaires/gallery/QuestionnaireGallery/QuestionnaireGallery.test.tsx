3f6a987e6fcae7946b5ef55f2861200fb7fd07b5
Dmitry Shagurov <shagurovda@gmail.com>
2022-09-03 13:28:11 +0400

    questionnaire gallery render test, tag gallery shanshot upd

---
+++ src/components/questionnaires/gallery/QuestionnaireGallery/QuestionnaireGallery.test.tsx
@@ -0,0 +1,67 @@
+import { rest } from "msw";
+import { setupServer } from "msw/node";
+import { screen, waitFor } from "@testing-library/react";
+import { renderWithProvidersAndRouters } from "src/utils/test-utils";
+import { setupStore } from "src/redux/store";
+import QuestionnaireGallery from "./QuestionnaireGallery";
+import { FetchStatus } from "src/models/FetchStatus";
+
+const cardsPlaceholder = [
+  {
+    "id": 1,
+    "label": "label1",
+    "tags": [1]
+  },
+  {
+    "id": 2,
+    "label": "label2",
+    "tags": [1]
+  },
+  {
+    "id": 3,
+    "label": "label3",
+    "tags": [1]
+  },
+  {
+    "id": 4,
+    "label": "label4",
+    "tags": [1]
+  },
+  {
+    "id": 5,
+    "label": "label5",
+    "tags": [1]
+  },
+]
+
+const server = setupServer(
+  rest.get(process.env.REACT_APP_BACKEND_URL + "questionnaires/questionnaireCards",
+    (_req, res, ctx) => {
+      return res(ctx.json({ questionnaires: cardsPlaceholder }));
+    }
+  )
+);
+
+const store = setupStore({
+  tags: {
+    tags: [
+      { id: 1, label: "TestTag1", freq: 7 }
+    ],
+    tagsLoadingStatus: FetchStatus.Complete
+  }
+});
+
+beforeAll(() => server.listen());
+afterEach(() => server.resetHandlers());
+afterAll(() => server.close());
+
+describe("QuestionnaireGallery component", () => {
+  test("QuestionnaireGallery load cards from api and renders",
+    async () => {
+      renderWithProvidersAndRouters(<QuestionnaireGallery />, { store });
+      await waitFor(
+        () => expect(screen.getAllByText(/label/i)).toHaveLength(5)
+      );
+      expect(screen.getAllByText(/testtag/i)).toHaveLength(5)
+    });
+});
