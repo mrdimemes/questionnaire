f4647fe89a9f3ca2e9536b9c1d0b01d2c1b23ab2
Dmitry Shagurov <shagurovda@gmail.com>
2022-09-07 15:42:44 +0400

    optimization for email regex (faster regex test)

--- src/utils/validators/validateByEmailRegex.ts
+++ src/utils/validators/validateByEmailRegex.ts
@@ -1,5 +1,5 @@
 const validateByEmailRegex = (email: string): string | undefined => {
-    const re = new RegExp(/^\w+([.-]?\w+)*@\w+([.-]?\w+)*(\.\w{2,3})+$/);
+    const re = new RegExp(/^\w+([.-]\w+)*@\w+([.-]\w+)*(\.\w{2,3})+$/);
     return re.test(email) ? undefined : "Некорректный адрес почты";
   }
 
