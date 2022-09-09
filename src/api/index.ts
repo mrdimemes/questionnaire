e2e9ec88efe0ce9d19a07daf1c113cd0b02d337e
Dmitry Shagurov <shagurovda@gmail.com>
2022-09-09 17:56:57 +0400

    api auth interceptors added; auth response models added

--- src/api/index.ts
+++ src/api/index.ts
@@ -1,8 +1,37 @@
 import axios from "axios";
+import { RefreshResponse } from "src/models/auth/RefreshResponse";
 
 const api = axios.create({
   withCredentials: true,
   baseURL: process.env.REACT_APP_BACKEND_URL
-})
+});
+
+api.interceptors.request.use(config => {
+  const accessToken = localStorage.getItem("accessToken");
+  if (accessToken) {
+    config.headers = { Authorization: `Bearer ${accessToken}` };
+  }
+  return config;
+});
+
+api.interceptors.response.use(
+  config => { return config },
+  async error => {
+    const originalRequest = error.config;
+    if (error.response.status === 401 &&
+      error.config &&
+      !error.config._isRetry
+    ) {
+      originalRequest._isRetry = true;
+      const response = await api.get<RefreshResponse>(
+        "auth/refresh",
+        { withCredentials: true }
+      );
+      localStorage.setItem("accessToken", response.data.accessToken);
+      localStorage.setItem("refreshToken", response.data.refreshToken);
+      return api.request(originalRequest);
+    }
+    throw error;
+  });
 
 export default api;
