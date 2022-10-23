3da6cdf6b4ba46c307f12117b51259110c14523b
= <shagurovda@gmail.com>
2022-10-23 14:38:05 +0400

    fixed withTheme HOC: non-theme className prop support, correct display name depends on wrapped component name

--- src/HOCs/withTheme.tsx
+++ src/HOCs/withTheme.tsx
@@ -1,9 +1,11 @@
-import React from "react";
+import { ComponentType } from "react";
+import classNames from "classnames";
 import { useThemeSelector } from "src/redux/hooks";
 import { Theme } from "src/redux/slices/themeSlice";
 
+
 type ClassNameProps = {
-  className: string;
+  className?: string;
 };
 
 type ThemeStyles = {
@@ -12,16 +14,23 @@ type ThemeStyles = {
 };
 
 export function withTheme<P>(
-  Component: React.ComponentType<P & ClassNameProps>,
+  Component: ComponentType<P & ClassNameProps>,
   styles: ThemeStyles,
 ) {
-  const ComponentWithTheme = (props: P) => {
+  const ComponentWithTheme = (props: P & ClassNameProps) => {
     const currentTheme = useThemeSelector();
     const themeClassName =
       currentTheme === Theme.Light ?
         styles.theme_light :
         styles.theme_dark;
-    return <Component {...props} className={themeClassName} />;
+    return (
+      <Component
+        {...props}
+        className={classNames(props.className, themeClassName)}
+      />
+    );
   };
+
+  ComponentWithTheme.displayName = Component.name + "WithTheme";
   return ComponentWithTheme;
 };
